8a661d0994c8046deff23874a6ba69851f9e4ef2
Avi K <avi1234@gmail.com>
2013-12-15 09:56:48 -0700

    add create book test

--- spec/features/integ2_spec.rb
+++ spec/features/integ2_spec.rb
@@ -3,10 +3,22 @@ require 'capybara/rspec'
 
 feature "Books" do
 
-  scenario "should show books page" do
+  scenario "show books page" do
   	visit '/books'
     expect(page).to have_content 'Listing books'
     print page.body
+  end
+
+  scenario "create new book" do
+  	visit '/books/new'
+    expect(page).to have_content 'New book'
+    within("#new_book") do
+      fill_in 'book_title', :with => 'H Potter'
+      fill_in 'book_author', :with => 'JK Rolling'
+    end
+    click_button 'Create Book'
+    print page.body
+    expect(page).to have_content 'Book was successfully created.'
   end
 
 end

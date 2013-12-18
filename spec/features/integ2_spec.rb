1eaaf28b19fb0912073271420dc73e75882ed8de
Avi K <avi1234@gmail.com>
2013-12-18 05:54:57 -0700

    bla

--- spec/features/integ2_spec.rb
+++ spec/features/integ2_spec.rb
@@ -6,19 +6,27 @@ feature "Books" do
   scenario "show books page" do
   	visit '/books'
     expect(page).to have_content 'Listing books'
-    print page.body
   end
 
   scenario "create new book" do
-  	visit '/books/new'
+  	visit '/books'
+    click_link 'New Book'
     expect(page).to have_content 'New book'
     within("#new_book") do
       fill_in 'book_title', :with => 'H Potter'
       fill_in 'book_author', :with => 'JK Rolling'
     end
     click_button 'Create Book'
-    print page.body
     expect(page).to have_content 'Book was successfully created.'
+
+    click_link 'Back'
+    expect(page).to have_content 'Listing books'
+
+    within("#books_list") do
+      expect(page).to have_content 'H Potter'
+      expect(page).to have_content 'JK Rolling'
+    end
+
   end
 
 end
--- spec/integ_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe 'integ tests' do
-  it "5 should eq 5" do
-    expect(5).to eq(5)
-  end
-end
